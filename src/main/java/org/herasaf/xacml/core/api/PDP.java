715b7749397fff94739294212aff52da69e3d147
René Eggenschwiler <reggensc@herasaf.org>
2009-07-22 11:26:02 +0000

    OPEN - issue HERASAFXACMLCORE-1: Transition of Code from old to new structure http://jira.herasaf.org/browse/HERASAFXACMLCORE-1
    git-svn-id: svn://localhost/herasaf-xacmlnew/trunk/herasaf-xacml/herasaf-xacml-core@1187 281314fa-7b5e-d943-bc7f-69d318bcf3fb

---
+++ src/main/java/org/herasaf/xacml/core/api/PDP.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2008 HERAS-AF (www.herasaf.org)
+ * Holistic Enterprise-Ready Application Security Architecture Framework
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.herasaf.xacml.core.api;
+
+import org.herasaf.xacml.core.context.RequestCtx;
+import org.herasaf.xacml.core.context.ResponseCtx;
+import org.herasaf.xacml.core.context.impl.RequestType;
+import org.herasaf.xacml.core.policy.Evaluatable;
+
+/**
+ * TODO JAVADOC!!!!!!!!
+ * 
+ * The {@link PDP} is used to evaluate a request provided by the context
+ * handler. The context handler sends the request to the {@link PDP}. The
+ * {@link PDP} evaluates the {@link RequestCtx} and returns a
+ * {@link ResponseCtx}.<br>
+ * See: <a href=
+ * "http://www.oasis-open.org/committees/tc_home.php?wg_abbrev=xacml#XACML20">
+ * OASIS eXtensible Access Control Markup Langugage (XACML) 2.0, Errata 29 June
+ * 2006</a> for further information. <br>
+ * <br>
+ * A Policy Decision Point can handle only one resource per {@link RequestType}.
+ * The context handler is responsible for splitting up a {@link RequestType}
+ * that contains multiple resources into many {@link RequestType}s. <br>
+ * See: <a href="See: <a href="http://docs.oasis-open.org/xacml/2.0/access_control-xacml-2.0-mult-profile-spec-os.pdf"
+ * >Multiple resource profile of XACML v2.0, 1 February 2005</a> for further
+ * information.
+ * 
+ * @author Florian Huonder
+ * @version 1.0
+ */
+public interface PDP {
+	/**
+	 * Evaluates a {@link RequestCtx} with the {@link Evaluatable}s stored in
+	 * the {@link PDP}.
+	 * 
+	 * @param request
+	 *            The {@link RequestCtx} to evaluate.
+	 * @return The {@link ResponseCtx} with the result of the evaluation.
+	 */
+	public ResponseCtx evaluate(RequestCtx request);
+}
---
+++ src/main/java/org/herasaf/xacml/core/api/PolicyRepository.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2008 HERAS-AF (www.herasaf.org)
+ * Holistic Enterprise-Ready Application Security Architecture Framework
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.herasaf.xacml.core.api;
+
+import java.util.Collection;
+
+import org.herasaf.xacml.core.EvaluatableNotFoundException;
+import org.herasaf.xacml.core.PolicyRepositoryException;
+import org.herasaf.xacml.core.SyntaxException;
+import org.herasaf.xacml.core.policy.Evaluatable;
+import org.herasaf.xacml.core.policy.EvaluatableID;
+
+/**
+ * The {@link PolicyRepository} 
+ * TODO JAVADOC!!!!
+ */
+public interface PolicyRepository {
+
+	/**
+	 * Retrives an {@link Evaluatable} from the local or a remote {@link PolicyRepository}.
+	 * 
+	 * @param id The id of the {@link Evaluatable}
+	 * @return The retrieved {@link Evaluatable}
+	 * @throws EvaluatableNotFoundException Thrown if the {@link Evaluatable} cannot be found.
+	 */
+	public Evaluatable getEvaluatable(EvaluatableID id)
+			throws PolicyRepositoryException;
+
+	/**
+	 * Deploys the given {@link Collection} of {@link Evaluatable}s to this
+	 * {@link PolicyRepository}.
+	 * 
+	 * @param evaluatables
+	 *            The {@link Collection} of {@link Evaluatable}s to deploy.
+	 * @throws DataIntegrityException
+	 * @throws DataAccessException
+	 * @throws SyntaxException 
+	 */
+	void deploy(Collection<Evaluatable> evaluatables)
+			throws PolicyRepositoryException, SyntaxException;
+
+	/**
+	 * Deploys the given {@link Evaluatable} to this {@link PolicyRepository}.
+	 * 
+	 * @param evaluatable
+	 *            The {@link Evaluatable} to deploy.
+	 * @throws DataAccessException
+	 * @throws DataIntegrityException
+	 * @throws SyntaxException 
+	 */
+	void deploy(Evaluatable evaluatable) throws PolicyRepositoryException, SyntaxException;
+
+	/**
+	 * Undeploys the {@link Evaluatable} identified by the given id.
+	 * 
+	 * @param id
+	 *            The id of the {@link Evaluatable} to undeploy.
+	 * @throws DataAccessException
+	 * @throws SyntaxException 
+	 */
+	void undeploy(EvaluatableID id) throws PolicyRepositoryException;
+
+	/**
+	 * Undeploys the {@link Evaluatable}s identified by the given
+	 * {@link Collection} ids.
+	 * 
+	 * @param ids
+	 *            The {@link Collection} of ids of the {@link Evaluatable}s.
+	 * @throws DataAccessException
+	 * @throws SyntaxException 
+	 */
+	void undeploy(Collection<EvaluatableID> ids) throws PolicyRepositoryException;
+}
