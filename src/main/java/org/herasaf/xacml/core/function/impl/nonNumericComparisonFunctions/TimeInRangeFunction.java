d6b190720d2ea5e0e08b15ec9b56f29a44971fd7
Florian Huonder <fhuonder@herasaf.org>
2012-11-10 18:39:35 +0000

    XACMLCORE-200
    git-svn-id: svn://localhost/XACMLCORE/trunk@3126 281314fa-7b5e-d943-bc7f-69d318bcf3fb

--- src/main/java/org/herasaf/xacml/core/function/impl/nonNumericComparisonFunctions/TimeInRangeFunction.java
+++ src/main/java/org/herasaf/xacml/core/function/impl/nonNumericComparisonFunctions/TimeInRangeFunction.java
@@ -24,15 +24,13 @@ import org.herasaf.xacml.core.types.Time;
 /**
  * <p>
  * The implementation of the
- * urn:oasis:names:tc:xacml:1.0:function:time-less-than function.
+ * urn:oasis:names:tc:xacml:2.0:function:time-less-than function.
  * </p>
  * <p>
  * See: Apendix A.3 of the <a href=
  * "http://www.oasis-open.org/committees/tc_home.php?wg_abbrev=xacml#XACML20">
  * OASIS eXtensible Access Control Markup Langugage (XACML) 2.0, Errata, 29 January 2008</a> page 118, for further information.
  * </p>
- * 
- * @author Stefan Oberholzer
  */
 public class TimeInRangeFunction extends AbstractFunction {
 
