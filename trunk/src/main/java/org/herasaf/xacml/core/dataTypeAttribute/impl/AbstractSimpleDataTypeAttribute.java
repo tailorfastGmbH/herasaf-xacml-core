a6b73369ec13df7543f475fb01faff57436a0887
Florian Huonder <fhuonder@herasaf.org>
2013-07-08 09:14:11 +0000

    https://herasaf.jira.com/browse/XACMLCORE-30
    git-svn-id: svn://localhost/XACMLCORE@3142 281314fa-7b5e-d943-bc7f-69d318bcf3fb

--- trunk/src/main/java/org/herasaf/xacml/core/dataTypeAttribute/impl/AbstractSimpleDataTypeAttribute.java
+++ trunk/src/main/java/org/herasaf/xacml/core/dataTypeAttribute/impl/AbstractSimpleDataTypeAttribute.java
@@ -4,20 +4,16 @@ import java.util.List;
 
 import org.herasaf.xacml.core.SyntaxException;
 
-public abstract class AbstractSimpleDataTypeAttribute<T> extends AbstractDataTypeAttribute<T> {
+public abstract class AbstractSimpleDataTypeAttribute<T> extends
+		AbstractDataTypeAttribute<T> {
 
 	private static final long serialVersionUID = 1L;
 
 	public T convertTo(List<?> jaxbRepresentation) throws SyntaxException {
-//		if (jaxbRepresentation.size() != 1) {
-//			throw new SyntaxException("Unexpected list size, should only contain 1 element.");
-//		}
 		Object simpleType = jaxbRepresentation.get(0);
-//		if (!(simpleType instanceof String)) {
-//			throw new SyntaxException("Elemenet should be a string, but is a " + simpleType.getClass().getName() + ".");
-//		}
 		return convertTo(((String) simpleType).trim());
 	}
-	
-	public abstract T convertTo(String jaxbRepresentation) throws SyntaxException;
+
+	public abstract T convertTo(String jaxbRepresentation)
+			throws SyntaxException;
 }
